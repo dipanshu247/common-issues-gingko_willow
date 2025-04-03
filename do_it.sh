b379802e61bf8611aab9361936e40cdcc0cb1bc1
dipanshu247 <dipanshu247kumar@gmail.com>
2025-04-04 00:42:09 +0530

    Update do_it.sh

--- do_it.sh
+++ do_it.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # 1. Clean up old files
-# echo "Cleaning up old files..."
-# rm -rf .repo/local_manifests
-# rm -rf {device,vendor,kernel,hardware}/xiaomi
+echo "Cleaning up old files..."
+rm -rf .repo/local_manifests
+rm -rf {device,vendor,kernel,hardware}/xiaomi
 # echo "Old files cleaned."
 
 # 4. Clone device-specific repositories
-# echo "Cloning device repositories..."
-# git clone https://github.com/LineageOS/android_device_xiaomi_ginkgo -b lineage-20 device/xiaomi/ginkgo
-# git clone https://github.com/Wrdn28/vendor_xiaomi_ginkgo -b lineage-20 vendor/xiaomi/ginkgo
-# git clone https://github.com/kutemeikito/android_kernel_xiaomi_ginkgo.git -b thirteen/main kernel/xiaomi/ginkgo
-# git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
-# echo "Device repositories cloned."
+echo "Cloning device repositories..."
+git clone https://github.com/LineageOS/android_device_xiaomi_ginkgo -b lineage-20 device/xiaomi/ginkgo/
+git clone https://github.com/LineageOS/android_device_xiaomi_sm6125-common.git device/xiaomi/sm6125-common/
 
-# git clone https://github.com/LineageOS/android_device_xiaomi_sm6125-common.git device/xiaomi/sm6125-common/
+git clone https://github.com/Wrdn28/vendor_xiaomi_ginkgo -b lineage-20 vendor/xiaomi/ginkgo/
+git clone https://github.com/sailfish-ginkgo/vendor_sm6125-common vendor/xiaomi/sm6125-common/
+
+git clone https://github.com/kutemeikito/android_kernel_xiaomi_ginkgo.git -b thirteen/main kernel/xiaomi/ginkgo/
+git clone https://github.com/LineageOS/android_kernel_xiaomi_sm6125 kernel/xiaomi/sm6125/
+
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi/
+echo "Device repositories cloned."
+
 
 
 # 5. Set environment variables
