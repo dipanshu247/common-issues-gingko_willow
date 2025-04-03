638c2e44f9e8e24a428cb59c64d1663e01d0abae
dipanshu247 <dipanshu247kumar@gmail.com>
2025-04-03 12:13:19 +0530

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,20 @@
+echo "Setting environment variables..."
+export BUILD_USERNAME=Dipanshu247
+export BUILD_HOSTNAME=crave
+export TZ="Asia/New Delhi"
+echo "Environment variables set."
+
+# 6. Setup the build environment
+echo "Setting up build environment..."
+source build/envsetup.sh
+echo "Build environment setup."
+
+# 7. Select the build target
+echo "Selecting build target..."
+lunch lineage_ginkgo-userdebug
+echo "Build target selected."
+
+# 8. Start the build
+echo "Starting the build..."
+mka bacon
+echo "Build complete!"
